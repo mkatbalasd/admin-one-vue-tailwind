16fe859ad72d38c24e52aad69b1e259ec5cc3c44
mkatbalasd <mkatbalasd@gmail.com>
2025-07-25 20:10:24 +0300

    chore: lint backend

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import globals from 'globals'
 
 export default [
   {
@@ -16,8 +17,14 @@ export default [
       '**/coverage/**',
       '**/.laravel-guide/**',
       '**/.nuxt-guide/**',
-      'backend/**',
     ],
+  },
+
+  {
+    files: ['backend/**/*.{js,mjs}'],
+    languageOptions: {
+      globals: globals.node,
+    },
   },
 
   js.configs.recommended,
