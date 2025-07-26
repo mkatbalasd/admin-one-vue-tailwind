f451a3ff64e88c6167df587cdba2c47aeb90abd6
mkatbalasd <mkatbalasd@gmail.com>
2025-07-26 23:19:05 +0300

    Add vue-hijri-picker and update HijriDatePicker component

--- src/components/HijriDatePicker.vue
+++ src/components/HijriDatePicker.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, watch } from 'vue'
-import VueDatePicker from '@vuepic/vue-datepicker'
-import '@vuepic/vue-datepicker/dist/main.css'
+import DatePicker from 'vue-hijri-picker'
+import 'vue-hijri-picker/dist/style.css'
 import moment from 'moment-hijri'
 
 const props = defineProps({
@@ -13,31 +13,31 @@ const props = defineProps({
 
 const emit = defineEmits(['update:modelValue'])
 
-const innerValue = ref(null)
+const innerValue = ref('')
 
 watch(
   () => props.modelValue,
   (val) => {
-    innerValue.value = val ? moment(val, 'iYYYY-iMM-iDD').toDate() : null
+    innerValue.value = val ? moment(val, 'iYYYY-iMM-iDD').toISOString() : ''
   },
   { immediate: true },
 )
 
-watch(
-  innerValue,
-  (val) => {
-    if (val) {
-      emit('update:modelValue', moment(val).format('iYYYY-iMM-iDD'))
-    } else {
-      emit('update:modelValue', '')
-    }
-  },
-)
+watch(innerValue, (val) => {
+  if (val) {
+    emit('update:modelValue', moment(val).format('iYYYY-iMM-iDD'))
+  } else {
+    emit('update:modelValue', '')
+  }
+})
 </script>
 
 <template>
-  <VueDatePicker
+  <DatePicker
     v-model="innerValue"
+    initialType="hijri"
+    language="ar"
+    format="iYYYY-iMM-iDD"
     :input-class="'px-3 py-2 h-12 w-full border rounded-sm bg-white dark:bg-slate-800 focus:ring-3 focus:outline-hidden'"
   />
 </template>
