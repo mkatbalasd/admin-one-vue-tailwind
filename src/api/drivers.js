d702892dcb6442b5d01ae27778a8eec5ccb8e3af
mkatbalasd <mkatbalasd@gmail.com>
2025-07-26 21:49:57 +0300

    Add vehicle API and extend driver/facility APIs

--- src/api/drivers.js
+++ src/api/drivers.js
@@ -2,3 +2,5 @@ import API from './api'
 
 export const getDrivers = () => API.get('/drivers')
 export const addDriver = (payload) => API.post('/drivers', payload)
+export const updateDriver = (id, payload) => API.put(`/drivers/${id}`, payload)
+export const deleteDriver = (id) => API.delete(`/drivers/${id}`)
