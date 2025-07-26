0ad2161e2c3b37e1edee48bcec47719ddd242b2e
mkatbalasd <mkatbalasd@gmail.com>
2025-07-26 18:27:04 +0300

    Replace deprecated tailwindcss-rtl

--- src/components/SectionTitleLineWithButton.vue
+++ src/components/SectionTitleLineWithButton.vue
@@ -25,8 +25,8 @@ const { t } = useI18n()
 <template>
   <section :class="{ 'pt-6': !main }" class="mb-6 flex items-center justify-between">
     <div class="flex items-center justify-start">
-      <IconRounded v-if="icon && main" :icon="icon" color="light" class="me-3" bg />
-      <BaseIcon v-else-if="icon" :path="icon" class="me-2" size="20" />
+      <IconRounded v-if="icon && main" :icon="icon" color="light" class="mr-3" bg />
+      <BaseIcon v-else-if="icon" :path="icon" class="mr-2" size="20" />
       <h1 :class="main ? 'text-3xl' : 'text-2xl'" class="leading-tight">
         {{ t(title) }}
       </h1>
